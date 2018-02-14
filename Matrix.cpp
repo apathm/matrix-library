45c8305af5ed2aebf48cb47eee15ea656d57e50a
apathm <patrick.ahmetovic@gmail.com>
2018-02-14 17:15:35 +0100

    Change indexing
    Change the loops to start from value one. Also replaced int values in the loops with size_t

--- Matrix.cpp
+++ Matrix.cpp
@@ -15,27 +15,27 @@ Matrix::Matrix(int rows, int columns, const vector<double>& v): rows{rows}, colu
 		this->set_values(v); 
 
 	else
-		for(int i{0}; i < rows; ++i)
-			for(int j{0}; j < columns; ++j)
+		for(size_t i{0}; i < rows; ++i)
+			for(size_t j{0}; j < columns; ++j)
 				m[i*columns + j] = 0;
 }
 
-Matrix::~Matrix(){ delete[] m; };
+Matrix::~Matrix(){ delete[] m; }
 
 Matrix::Matrix(const Matrix& original){
 	rows = original.rows;
 	columns = original.columns;
 
 	m = new double[rows*columns];
-	for(int i{0}; i < rows; ++i)
-		for(int j{0}; j < columns; ++j)
+	for(size_t i{0}; i < rows; ++i)
+		for(size_t j{0}; j < columns; ++j)
 			m[i*columns + j] = original.m[i*columns+j];
 }
 
 Matrix& Matrix::operator=(const Matrix& original){
 	double *mbuf = new double[original.rows * original.columns];
-	for(int i{0}; i < original.rows; ++i)
-		for(int j{0}; j < original.columns; ++j)
+	for(size_t i{0}; i < original.rows; ++i)
+		for(size_t j{0}; j < original.columns; ++j)
 			mbuf[i*original.columns + j] = original.m[i*original.columns+j];
 
 	if(m) delete[] m;
@@ -47,31 +47,31 @@ Matrix& Matrix::operator=(const Matrix& original){
 
 
 double& Matrix::at(int row, int column){
-	if(row <= 0 || this->rows < row)
+	if(row < 0 || row >= this->rows)
 		throw runtime_error("Matrix::at: variable row is out of range.");
 
-	if(column <= 0 || this->columns < column)
+	if(column < 0 || this->columns <= column)
 		throw runtime_error("Matrix::at: variable column is out of range.");
 
-	int index{(row-1)*columns + (column-1)};
+	int index{row*columns + column};
 	if(index < 0 || index > this->size()-1)
 		throw runtime_error("Matrix::at: out of range.");
 
-	return m[(row-1)*columns + (column-1)];
+	return m[row*columns + column];
 }
 
 const double& Matrix::at(int row, int column) const{
-	if(row <= 0 || this->rows < row)
+	if(row < 0 || this->rows <= row)
 		throw runtime_error("Matrix::at: variable row is out of range.");
 
-	if(column <= 0 || this->columns < column)
+	if(column < 0 || this->columns <= column)
 		throw runtime_error("Matrix::at: variable column is out of range.");
 
-	int index{(row-1)*columns + (column-1)};
+	int index{row*columns + column};
 	if(index < 0 || index > this->size()-1)
 		throw runtime_error("Matrix::at: out of range.");
 
-	return m[(row-1)*columns + (column-1)];
+	return m[row*columns + column];
 }
 
 size_t Matrix::size() const{
@@ -89,17 +89,17 @@ Matrix Matrix::set_values(const vector<double>& v){
 		
 	assert(temp.size() == this->size());
 
-	for(size_t i{1}; i <= rows; ++i)
-		for(size_t j{1}; j <= columns; ++j)
-			this->at(i,j) = temp.at((i-1)*columns + (j-1));	
+	for(size_t i{0}; i < rows; ++i)
+		for(size_t j{0}; j < columns; ++j)
+			this->at(i,j) = temp.at(i*columns + j);	
 
 	return *this;
 }
 
 
 Matrix& Matrix::multiply(int scalar){
-	for(size_t i{1}; i <= rows; ++i)
-		for(size_t j{1}; j <= columns; ++j)
+	for(size_t i{0}; i < rows; ++i)
+		for(size_t j{0}; j < columns; ++j)
 			this->at(i,j) *= scalar;
 
 	return *this;
@@ -111,11 +111,11 @@ Matrix& Matrix::multiply(const Matrix& m){
 
 	vector<double> temp;
 
-	for(int i{1}; i <= this->rows; ++i){
-		for(int j{1}; j <= m.columns; ++j){
+	for(size_t i{0}; i < this->rows; ++i){
+		for(size_t j{0}; j < m.columns; ++j){
 			double sum{0};
 
-			for(int k{1}; k <= m.rows; ++k)
+			for(size_t k{0}; k < m.rows; ++k)
 				sum += this->at(i, k) * m.at(k, j);
 
 			temp.push_back(sum);
@@ -132,8 +132,8 @@ Matrix& Matrix::add(const Matrix& m){
 	if(this->rows != m.rows || this->columns != m.columns)
 		throw runtime_error("Matrix::add: Matrices do not have same dimensions");
 
-	for(int i{1}; i <= rows; ++i)
-		for(int j{1}; j <= columns; ++j)
+	for(size_t i{0}; i < rows; ++i)
+		for(size_t j{0}; j < columns; ++j)
 			this->at(i, j) += m.at(i,j);
 
 	return *this;
@@ -143,8 +143,8 @@ Matrix& Matrix::substract(const Matrix& m){
 	if(this->rows != m.rows || this->columns != m.columns)
 		throw runtime_error("Matrix::add: Matrices do not have same dimensions");
 
-	for(int i{1}; i <= rows; ++i)
-		for(int j{1}; j <= columns; ++j)
+	for(size_t i{0}; i < rows; ++i)
+		for(size_t j{0}; j < columns; ++j)
 			this->at(i, j) -= m.at(i,j);
 
 	return *this;
@@ -166,8 +166,8 @@ Matrix& Matrix::operator*(const Matrix& m){
 Matrix& Matrix::transpose(){
 	vector<double> temp;
 
-	for(int i{1}; i <= columns; ++i)
-		for(int j{1}; j <= rows; ++j)
+	for(size_t i{0}; i < columns; ++i)
+		for(size_t j{0}; j < rows; ++j)
 			temp.push_back(this->at(j, i));
 	
 	Matrix m {this->columns, this->rows, temp};
@@ -178,11 +178,11 @@ Matrix& Matrix::transpose(){
 
 
 ostream& Matrix::print(ostream& o) const{
-	for(int i{1}; i <= rows; ++i){
+	for(size_t i{0}; i < rows; ++i){
 		o << "|";
 
 		bool first{true};
-		for(int j{1}; j <= columns; ++j){
+		for(size_t j{0}; j < columns; ++j){
 			if(first){
 				o << this->at(i, j);
 				first = false;
